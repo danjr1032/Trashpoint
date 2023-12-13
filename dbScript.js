7d37d0b521220630a3fbdc190519c01f6b585a3e
danjr1032 <tapjidan@gmail.com>
2023-12-13 11:00:45 +0100

    update to dbScript

--- dbScript.js
+++ dbScript.js
@@ -1,11 +1,9 @@
  //DIS PLAY NAME FUNCTION//
  const fullname = localStorage.getItem('fullname');
- const phone = localStorage.getItem('phone');
- 
+    const phone = localStorage.getItem('phone');
 
- const Name = document.querySelector('.fname');
- const Phone = document.querySelector('.phoneNumber');
+    const Name = document.querySelector('.fname');
+    const Phone = document.querySelector('.phoneNumber');
 
-
- Name.textContent =`Welcome ${fullname}`;
- Phone.textContent = phone;
+    Name.textContent = `Welcome ${fullname}`;
+    Phone.textContent = `Phone: ${phone}`;

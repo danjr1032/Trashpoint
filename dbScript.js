b011d2136850256bf243a66475c17d0e85c701f6
danjr1032 <tapjidan@gmail.com>
2023-12-13 12:21:18 +0100

    update

--- dbScript.js
+++ dbScript.js
@@ -1,11 +1,7 @@
 
 
-
- const fullName = localStorage.getItem('fullName');
+    const fullName = localStorage.getItem('fullName');
     const phone = localStorage.getItem('phone');
-
-    const Name = document.querySelector('.fname');
-    const Phone = document.querySelector('.phoneNumber');
-
-    Name.textContent = `Welcome ${fullName}`;
-    Phone.textContent = `Phone: ${phone}`;
+  
+    document.getElementById('fname').innerText = fullName;
+    document.getElementById('phoneNumber').innerText = phone;
